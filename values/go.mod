2f9c03b885dd920fcbd58ebcf754bd78558a694b
undesicimo <105863111+undesicimo@users.noreply.github.com>
2024-11-17 23:08:37 +0900

    Add go.mod file for module definition and Go version

---
+++ values/go.mod
@@ -0,0 +1,3 @@
+module example.com/values
+
+go 1.23.3
---
+++ values/values.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("go" + "lang")
+	fmt.Println("Hello World")
+	fmt.Println(true)
+	fmt.Println(false)
+}
